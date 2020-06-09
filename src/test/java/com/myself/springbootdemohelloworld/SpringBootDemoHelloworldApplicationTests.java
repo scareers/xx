1a8e2e2c6d7b630b33474a34b7e3e2c5e67c46f5
testname <12345678@qq.com>
2020-06-09 10:17:56 +0800

    first

---
+++ src/test/java/com/myself/springbootdemohelloworld/SpringBootDemoHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myself.springbootdemohelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
